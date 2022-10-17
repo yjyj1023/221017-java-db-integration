145490607fa2462022208ad067f78c6208aa3ac2
yjyj1023 <lyj19961023@gmail.com>
2022-10-17 13:55:13 +0900

    자바와 db연동하기 SELECT FROM 쿼리문 실행하기

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String id, String name, String password){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
