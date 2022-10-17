fc6a4c07675ceae700737c2d01e4d4dbe71e6905
yjyj1023 <lyj19961023@gmail.com>
2022-10-17 11:49:28 +0900

    자바와 db연동하기

---
+++ src/main/java/org/example/UserDao.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class UserDao {
+    public void add() throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(dbHost, dbUser,dbPassword);
+        PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?)");
+        ps.setString(1,"1");
+        ps.setString(2,"YeonJae");
+        ps.setString(3,"1123");
+
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+        System.out.println("DB연동 성공");
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        userDao.add();
+
+    }
+}
